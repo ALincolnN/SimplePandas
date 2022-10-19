d2d1f056b967bd8566fdd76a5553ba367d19d95d
ALincolnN <njoguabraham2@gmail.com>
2022-10-19 18:13:25 +0300

    Updated the Database Connection class adding the data Insert and Update method.

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -22,7 +22,29 @@ class DatabaseConnection:
             return record
 
         except (Exception, psycopg2.Error) as error:
-            print('Error while connecting to the database', error)
+            return 'Error while connecting to the database', error
+
+        finally:
+            try:
+                cursor.close()
+
+                connection.close()
+
+            except NameError:
+                return 'The connection had a failure while trying to connect to the database', NameError
+
+    def CreateUpdate(self):
+        try:
+            connection = psycopg2.connect()
+
+            cursor = connection.cursor()
+
+            cursor.execute(self.sql)
+
+            connection.commit()
+
+        except (Exception, psycopg2.Error) as error:
+            return 'Error while connecting to the database', error
 
         finally:
             try:
