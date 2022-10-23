c0fb7a12ed40ca8185108fbbcd69c4c2e292d291
ALincolnN <njoguabraham2@gmail.com>
2022-10-23 17:00:13 +0300

    Streamlined the code to import the Configs file and pass the contents better to the Main method. Also tested the test Sql to ensure that the cod works and it does.

--- Main.py
+++ Main.py
@@ -1,10 +1,26 @@
+from Configs import *
+
 import pandas as pd
+from DatabaseConnection import *
 
 
 class Loader:
 
     def __init__(self):
-        pass
+
+        self.conf = os.environ.get('Database Object')
 
     def main(self):
-        pass
+        test_sql = 'select usename as role_name from pg_catalog.pg_user'
+
+        result = DatabaseConnection(database_configuration=self.conf, sql_statement=test_sql)
+
+        conn = result.connection()
+
+        return conn
+
+
+x = Loader()
+print(x.main())
+
+
